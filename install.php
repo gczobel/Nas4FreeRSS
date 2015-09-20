2d35dbfb3926c59831694742d91e386ce13d57f1
gczobel <gczobel@gmail.com>
2015-09-20 17:47:04 +0300

    updated install / uninstall

--- install.php
+++ install.php
@@ -9,8 +9,8 @@ require_once('updatenotify.inc');
 $WWWPATH = '/usr/local/www/';
 $INSTALLPATH = getcwd() . '/';
 
-echo 'Extracting RSS.tgz', "\n";
-exec('tar -xzf RSS.tgz');
+#echo 'Extracting RSS.tar.gz' . PHP_EOL;
+#exec('tar -xzf RSS.tar.gz');
 
 if (!is_dir($WWWPATH.'ext')) mkdir($WWWPATH.'ext');
 
@@ -18,109 +18,47 @@ if (!is_dir($WWWPATH.'ext')) mkdir($WWWPATH.'ext');
 exec("rm {$WWWPATH}extension_rss_*");
 exec("rm -rf {$WWWPATH}ext/RSS");
 
-foreach (glob('www/*.php') as $filename) {
-    echo 'Linking ', $filename, ' as ', $WWWPATH, 'extension_', basename($filename), "\n";
+foreach (glob('www/*.{php,inc}', GLOB_BRACE) as $filename) {
+    echo "Linking {$filename} as {$WWWPATH}extension_" . basename($filename) . PHP_EOL;
     symlink($INSTALLPATH . $filename, $WWWPATH . 'extension_' . basename($filename));
 }
 
-echo 'Linking ext directory', "\n";
+echo 'Linking ext directory' . PHP_EOL;
 symlink($INSTALLPATH . 'www/ext/RSS', $WWWPATH.'ext/RSS');
 
-$min = date('i');
-if ($min > 14) $min %= 15;
-
-if (!is_array($config['cron']))
-   $config['cron'] = array();
-
-if (!is_array($config['cron']['job']))
-    $config['cron']['job'] = array();
-
-echo 'Looking for cron job...';
-$found = false;
-foreach ($config['cron']['job'] as &$job) {
-    if (preg_match('/(?:^|\s)RSS(?:$|\s)/i', $job['desc'])) {
-        if (preg_match('/.*\.php$/', $job['command'])) {
-            $job['command'] = $INSTALLPATH . 'sys/cron.sh';
-            echo "updated\n";
-        } else {
-				    echo "found\n";
-        }
-        $found = true;
-        break;
-    }
-}
-
-if (!$found) {
-    echo 'creating', "\n";
-    $cronjob = array();
-    $cronjob['enable'] = true;
-    $cronjob['uuid'] = uuid();
-    $cronjob['desc'] = 'RSS Cron Job';
-    $cronjob['minute'] = array($min, $min + 15, $min + 30, $min + 45);
-    $cronjob['hour'] = array();
-    $cronjob['day'] = array();
-    $cronjob['month'] = array();
-    $cronjob['weekday'] = array();
-    $cronjob['all_mins'] = '0';
-    $cronjob['all_hours'] = '1';
-    $cronjob['all_days'] = '1';
-    $cronjob['all_months'] = '1';
-    $cronjob['all_weekdays'] = '1';
-    $cronjob['who'] = 'root';
-    $cronjob['command'] = $INSTALLPATH . 'sys/cron.sh';
-
-    $config['cron']['job'][] = $cronjob;
-}
-
 if (!is_array($config['rss'])) {
-    echo 'Initializing configuration', "\n";
+    echo 'Initializing configuration' . PHP_EOL;
     $config['rss'] = array(
         'feeds' => array('rule' => array()),
         'filters' => array('rule' => array())
     );
 }
 
-# Clearing cookies
-foreach ($config['rss']['feeds']['rule'] as &$feed) if (!isset($feed['cookie'])) $feed['cookie'] = '';
+# Clearing old cookies
+foreach ($config['rss']['feeds']['rule'] as &$feed)
+	if (!isset($feed['cookie'])) $feed['cookie'] = '';
 
 # Set install path
 $config['rss']['path'] = $INSTALLPATH;
 
 # Creating rc script to copy files on NAS boot.
-echo 'Looking for rc command...';
-$found = false;
-if (!is_array($config['rc']))
-   $config['rc'] = array();
+$config['rc']['postinit']['cmd'] = '/usr/local/bin/php-cgi ' . $INSTALLPATH . 'sys/rss_start.php';
 
-if (!is_array($config['rc']['postinit']))
-   $config['rc']['postinit'] = array();
+#Put default values
+$config['rss']['rpcpath'] = "/transmission/rpc";
+$config['rss']['pause_time'] = 100;
 
-foreach ($config['rc']['postinit'] as &$cmd) {
-   if (preg_match('/(?:^|\s)rss_start(?:$|\s)/i', $cmd)) {
-       echo "updating\n";
-       $found = true;
-       break;
-   }
-}
+$config['rss']['notifications'] = false;
+$config['rss']['report_email'] = $config['system']['email']['from'];
 
-if (!$found) {
-    echo 'creating', "\n";
-    $config['rc']['postinit']['cmd'] = '/usr/local/bin/php-cgi' . $INSTALLPATH . 'sys/rss_start.php';
-}
+$config['rss']['history_pagination'] = true;
+$config['rss']['history_pagination_limit'] = 20;
+
+$config['rss']['debug'] = "4";
 
 # Save configuration files.
-echo 'Saving configuration changes', "\n";
+echo 'Saving configuration changes' . PHP_EOL;
 write_config();
-
-if (!file_exists($g['varrun_path'] . '/sysreboot.reqd') && $found == false) {
-    echo 'Updating cron service', "\n";
-    $retval |= updatenotify_process('cronjob', 'cronjob_process_updatenotification');
-    config_lock();
-    $retval |= rc_update_service('cron');
-    config_unlock();
-}
-else if ($found == false)
-    echo 'Nas4Free is requesting a reboot before starting new cron jobs', "\n";
 
 echo "
 
--- uninstall.php
+++ uninstall.php
@@ -18,35 +18,38 @@ function is_dir_empty($dir) {
 $INSTALLPATH = $config['rss']['path'];
 $WWWPATH = '/usr/local/www/';
 
-echo 'Removing RSS settings', PHP_EOL;
-unset($config['rss']);
+if (isset($config['rss']) && isset($config['rss']['feeds'])) {
+	#Get cron uuid from feeds
+	foreach	($config['rss']['feeds']['rule'] as $feed) {
+		if (isset($feed['schedule_uuid'])) {
+			#
 
-#Remove Cron job
-echo 'Looking for cron...', PHP_EOL;
-if (is_array($config['cron'])) {
-   if (!is_array($config['cron']['job'])) {
-      echo 'No cron jobs to remove.', PHP_EOL;
-   } else {
-      echo 'Looking for job...', PHP_EOL;
-      foreach ( $config['cron']['job'] as $key=> $job) {
-         if ($job['desc'] === 'RSS Cron Job') {
-            echo "Removing job...", PHP_EOL;
-            unset($config['cron']['job'][$key]);
-            write_config();
-         }
-      }
-   }
+			echo 'Looking for job...', PHP_EOL;
+			foreach ( $config['cron']['job'] as $key=> $job) {
+				if ($job['uuid'] === $feed['schedule_uuid']) {
+					echo "Removing cron...'{$job['desc']}' uuid=" . $feed['schedule_uuid'] . PHP_EOL;
+					unset($config['cron']['job'][$key]);
+				}
+			}
+		}
+		
+	}	
 }
 
+#Remove config
+echo 'Removing RSS settings' . PHP_EOL;
+unset($config['rss']);
+write_config();
+
 #Remove RC script
-echo 'Looking for rc command...', PHP_EOL;
+echo 'Looking for rc command...' . PHP_EOL;
 if (is_array($config['rc'])) {
    if (!is_array($config['rc']['postinit'])) {
-      echo 'No rc command to remove.', PHP_EOL;
+      echo 'No rc command to remove.' . PHP_EOL;
    } else {
       foreach ( $config['rc']['postinit'] as $key=> $cmd) {
          if (strpos($cmd, 'sys/rss_start.php') !== FALSE) {
-            echo "Removing command...", PHP_EOL;
+            echo "Removing command..." . PHP_EOL;
             unset($config['rc']['postinit'][$key]);
             write_config();
          }
@@ -67,7 +70,7 @@ if (file_exists($linkfile) and is_dir($linkfile)) {
 }
 
 #Unlink php files on www folder.
-foreach (glob($WWWPATH . 'extension_rss_*.php') as $linkfile) {
+foreach (glob($WWWPATH . 'extension_rss_*{php,inc}', GLOB_BRACE) as $linkfile) {
    if (file_exists($linkfile)) {
       if(is_link($linkfile)) {
          echo "Removing symbolic link $linkfile", PHP_EOL;
@@ -82,7 +85,6 @@ if (is_dir_empty($WWWPATH.'ext')) {
    echo "Removing 'ext' folder", PHP_EOL;
    exec('rm -rf ' . $WWWPATH .'ext');
 }
-
 echo PHP_EOL, "Done.", PHP_EOL, "For a complete uninstall, remove the files on the directory '$INSTALLPATH' running the command 'rm -rf $INSTALLPATH'" , PHP_EOL
 
 ?>
